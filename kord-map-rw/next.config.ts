3dbd88eddf7d70a4de73165d30fc559ff768dd33
Kalle Leskinen <kalle@leskinen.org>
2026-08-17 21:43:32 +0300

    Update next.config.ts

--- kord-map-rw/next.config.ts
+++ kord-map-rw/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/:path*', // Catches every single URL (e.g. /customs, /settings)
-        destination: 'https://kordmap.wiki/:path*', // Forwards them to your new domain
+        destination: 'https://kordmap.wiki', // Forwards them to your new domain
         permanent: true, // 301 Permanent Redirect (Great for SEO and transferring Google rankings!)
       },
     ];
